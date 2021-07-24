0eb38d602f492f1eee02db0c347e44a5be20b90a
Damian <d.janas13@gmail.com>
2021-07-24 22:27:52 +0200

    Update app.ts

--- app1/src/app.ts
+++ app1/src/app.ts
@@ -6,5 +6,7 @@ export class App {
   public message = 'Hello World!';
 
   // @ts-ignore
-  m = import('app2/superFunction');
+  m = import('app2/superFunction').then(m => m.superFunction());
+  // @ts-ignore
+  p = import('app2/hello');
 }
